f1e1298851baee58d974b79fa4fc9fb385f4653a
HawkinsJM <hawkins.jeffm@gmail.com>
2026-04-26 23:29:33 -0700

    make env loading compatible with hosted environments

--- 02-node-express-api/server.js
+++ 02-node-express-api/server.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-process.loadEnvFile('.env');
+// loads the .env file if present, fails silently if it's not
+try {
+  process.loadEnvFile(".env");
+} catch {}
 
 const app = express();
 const port = Number(process.env.PORT || 3000);
 const server = app.listen(port);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/api/cat', async (req, res) => {
-  const response = await fetch('https://api.thecatapi.com/v1/images/search', {
-    headers: { 'x-api-key': process.env.CAT_API_KEY }
+app.get("/api/cat", async (req, res) => {
+  const response = await fetch("https://api.thecatapi.com/v1/images/search", {
+    headers: { "x-api-key": process.env.CAT_API_KEY }
   });
   const data = await response.json();
   res.json(data[0]);
